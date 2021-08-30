33396b3254f9c0f99a03a34a945798440b870469
Dang Minh Thinh <d-thinh@ruby-dev.jp>
2021-08-31 01:42:46 +0700

    Add Lien minh

--- javascript/application.js
+++ javascript/application.js
@@ -1,27 +1,70 @@
-function RunExcelJSExport() {
-  var dataField = document.getElementById('data-field');
+$(function () {
+  var pageTitle = window.location.href.match(/#(\w*)/) ? window.location.href.match(/#(\w*)/)[1] : 'coopSale';
+  var downloadFields = {
+    coopSale: [
+      ['name'],
+      ['email', 'phone'],
+      ['address'],
+      ['description'],
+      ['backup_name', 'backup_phone', 'backup_description'],
+      ['backup_address'],
+      ['product_name'],
+      ['category', 'category_type'],
+      ['avg'],
+      ['price_departure'],
+      ['price_destination']
+    ],
+    coopBuy: [['name'], ['email', 'phone'], ['description'], ['address'], ['product_name'], ['category', 'category_type']],
+    lmhtxSale: [['nguoiBanHangTenDonVi', 'nguoiBanHang'], ['nguoiBanHangDienThoai'], ['tenHang'], ['giaBanHTX'], ['giaBanGD']],
+    lmhtxBuy: [['nguoiMuaHang', 'nguoiMuaHangTenDonVi'], ['nguoiMuaHangDienThoai'], ['tenDonVi'], ['tenHang']]
+  };
+
+  $('.dropdown-menu .dropdown-item').on('click', function () {
+    pageTitle = this.href.match(/#(\w*)/)[1];
+    swicthPage(pageTitle);
+  });
+
+  $('#downloadFile').on('click', function () {
+    RunExcelJSExport(downloadFields[pageTitle]);
+  });
+});
+
+function swicthPage(page) {
+  var titleHead = {
+    coopSale: 'CoopLink Cần bán',
+    coopBuy: 'CoopLink Cần mua',
+    lmhtxSale: 'Liên minh Cần bán',
+    lmhtxBuy: 'Liên minh Cần mua'
+  };
+  $('.guide-zone').empty().append(`<h1 class="d-flex justify-content-center">${titleHead[page]}</h1>`);
+}
+
+function RunExcelJSExport(fields) {
   var workbook = new ExcelJS.Workbook();
-  const worksheet = workbook.addWorksheet('Sheet 1');
-  var jsonData = JSON.parse(dataField.value);
+  var worksheet = workbook.addWorksheet('Sheet 1');
+  var jsonData = JSON.parse($('#data-field').val());
   for (var key in jsonData) {
-    worksheet.addRow([
-      parseInt(key) + 1,
-      jsonData[key]['name'],
-      jsonData[key]['email'],
-      jsonData[key]['address'],
-      jsonData[key]['description'].replace(/(<([^>]+)>)/gi, '').replace(/\&nbsp;/g, ''),
-      [jsonData[key]['backup_name'], jsonData[key]['backup_phone'], jsonData[key]['backup_description']].join(' - '),
-      jsonData[key]['backup_address'],
-      jsonData[key]['product_name'],
-      [jsonData[key]['category'], jsonData[key]['category_type']].join(' / '),
-      jsonData[key]['avg'],
-      jsonData[key]['price_departure'],
-      jsonData[key]['price_destination'],
-    ]);
+    worksheet.addRow(fillData(jsonData, key, fields));
   }
-  debugger;
   var stamp = Date.now();
   workbook.xlsx.writeBuffer().then(function (buffer) {
     saveAs(new Blob([buffer], { type: 'application/octet-stream' }), `htx_${stamp}.xlsx`);
   });
+}
+
+function fillData(jsonData, key, fields) {
+  arr = [parseInt(key) + 1];
+  for (var str of fields) {
+    arr.push(
+      str
+        .map((text) =>
+          jsonData[key][text]
+            ?.toString()
+            .replace(/(<([^>]+)>)/gi, '')
+            .replace(/\&nbsp;/g, '')
+        )
+        .join(' \n')
+    );
+  }
+  return arr;
 }
